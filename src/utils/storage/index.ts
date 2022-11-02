b4e3d14874865fd481c65cde8d2ce95824b93d0d
João Pedro <joaopedromoraesnsilva@gmail.com>
2022-11-02 18:08:08 -0300

    feat: add storage functions

---
+++ src/utils/storage/index.ts
@@ -0,0 +1,26 @@
+const addAuthStorage = (token: string): void => {
+    const tokenExpiryTime = new Date().setHours(new Date().getHours() + 2).toString();
+    localStorage.setItem("tokenLetmeask", token);
+    localStorage.setItem("tokenExpiryTimeLetmeask", tokenExpiryTime);
+}
+
+const removeAuthStorage = (): void => {
+    localStorage.removeItem("tokenLetmeask");
+    localStorage.removeItem("tokenExpiryTimeLetmeask");
+}
+
+const getAuthStorage = (): { token: string | null, tokenExpiryTime: string | null } => {
+    const token = localStorage.getItem("tokenLetmeask");
+    const tokenExpiryTime = localStorage.getItem("tokenExpiryTimeLetmeask");
+
+    return {
+        token,
+        tokenExpiryTime
+    }
+} 
+
+export const storage = {
+    addAuthStorage,
+    removeAuthStorage,
+    getAuthStorage
+}
