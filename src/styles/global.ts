04c81e6dabc36b3cc90e7718f15a528a643c6735
João Pedro <joaopedromoraesnsilva@gmail.com>
2022-11-07 22:07:31 -0300

    feat: global style

---
+++ src/styles/global.ts
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+* {
+	margin: 0;
+	padding: 0;
+	box-sizing: border-box;
+}
+body {
+	font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+	background-color: #F8F8F8;
+}`;
