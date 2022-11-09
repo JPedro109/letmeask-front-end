344fc16b5e84716f29b1606ffdb9b0d72d26ca85
João Pedro <joaopedromoraesnsilva@gmail.com>
2022-11-09 18:41:08 -0300

    refactor: add auth and modal providers in app

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,9 @@ import { BrowserRouter } from "react-router-dom";
 import { AppContainer } from "./styles/app";
 import { GlobalStyle } from './styles/global';
 
+import { AuthProvider } from './providers/AuthProvider';
+import { ModalProvider } from './providers/ModalProvider';
+
 import { AppRoutes } from "./routes";
 
 const App = () => {
@@ -14,9 +17,13 @@ const App = () => {
       <BrowserRouter>
         <GlobalStyle />
         <ToastContainer />
+        <ModalProvider>
+          <AuthProvider>
             <AppContainer>
               <AppRoutes />
             </AppContainer>
+          </AuthProvider>
+        </ModalProvider>
       </BrowserRouter>
     </>
   );
