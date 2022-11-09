55add4f44c65b8f0d6878ac3d2fab5a058a9cb64
João Pedro <joaopedromoraesnsilva@gmail.com>
2022-11-09 18:39:00 -0300

    feat: api service

---
+++ src/services/api.ts
@@ -0,0 +1,23 @@
+import { storage } from "../utils/storage";
+
+import axios from "axios";
+
+const { token } = storage.getAuthStorage();
+
+const configApi = axios.create();
+
+console.log(import.meta.env.VITE_API_URL);
+
+configApi.defaults.baseURL = import.meta.env.VITE_API_URL;
+
+configApi.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+
+const addTokenInAuthorizationHeader = (token: string) => configApi.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+
+const cleanAuthorizationHeader = () => configApi.defaults.headers.common["Authorization"] = undefined;
+
+export const api = {
+  configApi,
+  addTokenInAuthorizationHeader,
+  cleanAuthorizationHeader
+}
