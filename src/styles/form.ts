6ed224c0c13715b242e914481128c087da009237
João Pedro <joaopedromoraesnsilva@gmail.com>
2022-11-09 18:53:28 -0300

    feat: form style

---
+++ src/styles/form.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Form = styled.form`
+    width: 100%;
+    padding: 1rem;
+    text-align: center;
+
+    > div, h2 {
+        margin-bottom: 1rem;
+    }
+`;
