24bab354ea66a1b2d652e87b5ca88fca49b7f8c0
João Pedro <joaopedromoraesnsilva@gmail.com>
2022-11-09 18:29:33 -0300

    refactor: add routes, toast container and styles in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+
+import { BrowserRouter } from "react-router-dom";
+
+import { AppContainer } from "./styles/app";
+import { GlobalStyle } from './styles/global';
+
+import { AppRoutes } from "./routes";
+
 const App = () => {
   return (
-    <div>
-      Vite
-    </div>
-  )
+    <>
+      <BrowserRouter>
+        <GlobalStyle />
+        <ToastContainer />
+            <AppContainer>
+              <AppRoutes />
+            </AppContainer>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
