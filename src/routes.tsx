79d80f08d7b257eddf4565d257b59a636d92d890
João Pedro <joaopedromoraesnsilva@gmail.com>
2022-11-09 18:49:01 -0300

    feat: not found page

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 
+import { PageNotFound } from "./pages/PageNotFound";
+
 export const AppRoutes = () => {
     return (
         <>
             <Routes>
                 <Route path="/" element={<h1>Vite</h1>} />
+                <Route path="/*" element={<PageNotFound />} />
             </Routes>
         </>
     );
