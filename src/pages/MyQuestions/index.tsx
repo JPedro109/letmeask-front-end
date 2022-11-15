f58c02be846c48c3278acd734c8912af89118505
João Pedro <joaopedromoraesnsilva@gmail.com>
2022-11-15 16:08:26 -0300

    refactor: add initial loading

--- src/pages/MyQuestions/index.tsx
+++ src/pages/MyQuestions/index.tsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
 
 import { Header } from "../../components/Header";
 import { QuestionComponent } from '../../components/Question';
@@ -12,63 +11,70 @@ import { api } from "../../services/api";
 
 import { QuestionTypes } from "../../types";
 import { notification } from '../../utils/notification';
+import { LoadingBigGif } from '../../components/LoadingBigGif';
 
 export const MyQuestions = () => {
     const [questions, setQuestions] = useState([]);
-    const navigate = useNavigate();
+    const [initialLoading, setInitialLoading] = useState<boolean>(true);
 
     useEffect(() => {
         let mounted = true;
-    
-        if(mounted) {
+
+        if (mounted) {
             const fetchQuestions = async () => {
                 await api
-                  .configApi
-                  .get(`/question`)
-                  .then(({ data }) => (setQuestions(data.response)))
-                  .catch(() => {
-                      notification.error("Erro no servidor, as perguntas não podem ser apresentadas");
-                      navigate("/create-room");
-                  });
-              };
+                    .configApi
+                    .get(`/question`)
+                    .then(({ data }) => (setQuestions(data.response)))
+                    .catch(() => {
+                        notification.error("Erro no servidor, as perguntas não podem ser apresentadas");
+                    });
+            };
 
             fetchQuestions();
 
+            setTimeout(() => setInitialLoading(false), 500);
         }
-        
+
         return () => {
             mounted = false;
             return;
         }
-    }, [questions, notification, navigate]);
+    }, [questions, notification]);
 
-    return ( 
+    return (
         <>
             <Header />
             <ContainerPage>
 
                 {
-                    questions.length === 0 ? (
-                        <InformationContainer>
-                            <img src={EmptyQuestions} alt="Imagem representando balões de pergunta" />
-                            <h2>Nenhuma pergunta por aqui</h2>
-                            <span>
-                                Entre em uma sala e comece a perguntar!
-                            </span>
-                        </InformationContainer>
-                    ) :
-
-                    questions.map((question: QuestionTypes) => (
-                        (
-                            <QuestionComponent 
-                                key={question.id} 
-                                questionObject={question} 
-                            />
-                        )
-                    ))
+                    initialLoading ? (
+                        <div style={{ display: "flex", justifyContent: "center", marginTop: "100px" }}>
+                            <LoadingBigGif></LoadingBigGif>
+                        </div>
+                    ) : (
+                        questions.length === 0 ? (
+                            <InformationContainer>
+                                <img src={EmptyQuestions} alt="Imagem representando balões de pergunta" />
+                                <h2>Nenhuma pergunta por aqui</h2>
+                                <span>
+                                    Entre em uma sala e comece a perguntar!
+                                </span>
+                            </InformationContainer>
+                        ) :
+    
+                            questions.map((question: QuestionTypes) => (
+                                (
+                                    <QuestionComponent
+                                        key={question.id}
+                                        questionObject={question}
+                                    />
+                                )
+                            ))
+                    )
                 }
 
             </ContainerPage>
         </>
-     );
+    );
 }
